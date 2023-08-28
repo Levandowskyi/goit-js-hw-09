969f1d0350bc5871dc4431b4af6a32854dafc177
Levandowskyi <stcorvin@gmail.com>
2023-08-28 12:39:13 +0300

    timer tsk2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,55 +1,68 @@
-const flatpickr = require("flatpickr");
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
-import flatpickr from "flatpickr"
+const elements = {
+  input: document.querySelector('#datetime-picker'),
+  button: document.querySelector('[data-start]'),
+  day: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
 
-const datePicker = flatpickr("#datetime-picker");
+elements.button.disabled = true;
+let currentDate = new Date().getTime();
 
-
-
-    const startButton = document.querySelector('[data-start]');
-    startButton.addEventListener('click', () => {
-      const selectedDate = datePicker.selectedDates[0];
-      const countdownInterval = setInterval(() => {
-        const currentDate = new Date();
-        const timeDifference = selectedDate - currentDate;
-        
-        if (timeDifference <= 0) {
-          clearInterval(countdownInterval);
-          updateTimerDisplay(0, 0, 0, 0);
-        } else {
-          const timeObject = convertMs(timeDifference);
-          updateTimerDisplay(timeObject.days, timeObject.hours, timeObject.minutes, timeObject.seconds);
-        }
-      }, 1000);
-    });
-
-    function convertMs(ms) {
-        
-        const second = 1000;
-        const minute = second * 60;
-        const hour = minute * 60;
-        const day = hour * 24;
-      
-        
-        const days = Math.floor(ms / day);
-        
-        const hours = Math.floor((ms % day) / hour);
-        
-        const minutes = Math.floor(((ms % day) % hour) / minute);
-        
-        const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-      
-        return { days, hours, minutes, seconds };
-      }
-      
-
-    function addLeadingZero(value) {
-      return value.toString().padStart(2, '0');
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectDate = selectedDates[0];
+    if (selectDate - currentDate < 0) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+    } else {
+      elements.button.disabled = false;
+      elements.input.disabled = true;
+      elements.button.addEventListener('click', handlerClick);
     }
+  },
+};
 
-    function updateTimerDisplay(days, hours, minutes, seconds) {
-      document.querySelector('[data-days]').textContent = addLeadingZero(days);
-      document.querySelector('[data-hours]').textContent = addLeadingZero(hours);
-      document.querySelector('[data-minutes]').textContent = addLeadingZero(minutes);
-      document.querySelector('[data-seconds]').textContent = addLeadingZero(seconds);
-    }
+const calendars = flatpickr(elements.input, options);
+
+function formatTime(value) {
+  return value.toString().padStart(2, '0');
+}
+
+function handlerClick() {
+  elements.button.disabled = true;
+  let selectDate = new Date(elements.input.value).getTime();
+  let ms = selectDate - currentDate;
+  let id;
+  if (ms > 0) {
+    id = setInterval(() => {
+      ms -= 1000;
+      elements.day.textContent = formatTime(convertMs(ms).days);
+      elements.hours.textContent = formatTime(convertMs(ms).hours);
+      elements.minutes.textContent = formatTime(convertMs(ms).minutes);
+      elements.seconds.textContent = formatTime(convertMs(ms).seconds);
+    }, 1000);
+  } else {
+    clearInterval(id);
+  }
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return { days, hours, minutes, seconds };
+}
